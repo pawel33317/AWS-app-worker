1dfefa896e71331290c515bbd965f91ff9329523
Ubuntu <ubuntu@ip-172-31-16-48.us-west-2.compute.internal>
2015-06-08 16:04:49 +0000

    za duzo by pisac
    Signed-off-by: Ubuntu <ubuntu@ip-172-31-16-48.us-west-2.compute.internal>

--- app.js
+++ app.js
@@ -16,68 +16,107 @@ var linkKolejki = tablicaKolejki.QueueUrl
 //obiekt kolejki z aws-sdk
 var sqs=new AWS.SQS();
 
-
 //obiekt do obsługi simple DB z aws-sdk
 var simpledb = new AWS.SimpleDB();
 
-
+//GraphicsMagic
+var gm = require('gm');
 //funkcja - petla wykonuje sie caly czas
 var myServer = function(){
 	
+	//parametr do funkcji pobierającej wiadomość z kolejki
 	var params = {
-	  QueueUrl: linkKolejki,
-	  AttributeNames: ['All'],
-	  MaxNumberOfMessages: 1,
-	  MessageAttributeNames: ['key','bucket'],
-	  VisibilityTimeout: 5,//na tyle sec nie widac jej w kolejce
-	  WaitTimeSeconds: 0//to na 0 
+		QueueUrl: linkKolejki,
+		AttributeNames: ['All'],
+		MaxNumberOfMessages: 1,
+		MessageAttributeNames: ['key','bucket'],
+		VisibilityTimeout: 10,//na tyle sec nie widac jej w kolejce
+		WaitTimeSeconds: 0//to na 0 
 	};
+	
+	//odbiera wiadomość
 	sqs.receiveMessage(params, function(err, data) {
-		if (err) {
-			console.log(err, err.stack); // an error occurred
-		}
-		else {
-			//console.log(JSON.stringify(data, null, 4));
+	if (err) {
+		console.log(err, err.stack); // an error occurred
+	}
+	else {
+		//console.log(JSON.stringify(data, null, 4));
+		
+		//Czy jest jakaś wiadomość
+		if(!data.Messages) {
+			console.log("No message in queue.");
+		} else {
 			
-			//Czy jest jakaś wiadomość
-			if(!data.Messages) {
-				console.log("No message in queue.");
-			} else {
+			//pobranie danych z body wiadomosci w kolejce i zrobienie z nich tablicy
+			//handler do ussunięcia wiadomości z kolejki
+			var ReceiptHandle_forDelete = data.Messages[0].ReceiptHandle;
+			//{bucket, key}
+			var messageinfo = JSON.parse(data.Messages[0].Body);
+			console.log("Otrzymano wiadomosc: bucket - "+messageinfo.bucket+", key - "+messageinfo.key);
+			
+			//to samo co wyzej tylko pobiera dane z metadanych a nie z body
+			//var messageinfo = { "bucket":data.Messages[0].MessageAttributes.bucket.StringValue,"key":data.Messages[0].MessageAttributes.key.StringValue}console.log(messageinfo.bucket);
 				
-				//pobranie danych z body wiadomosci w kolejce i zrobienie z nich tablicy
-				var ReceiptHandle_forDelete = data.Messages[0].ReceiptHandle;
-				var messageinfo = JSON.parse(data.Messages[0].Body);
-				console.log("Otrzymano wiadomosc: bucket - "+messageinfo.bucket+", key - "+messageinfo.key);
-				//to samo co wyzej tylko pobiera dane z metadanych a nie z body
-				//var messageinfo = { "bucket":data.Messages[0].MessageAttributes.bucket.StringValue,"key":data.Messages[0].MessageAttributes.key.StringValue}console.log(messageinfo.bucket);
+			//parametry do pobrania pliku (obiektu)
+			var params2 = {
+				Bucket: messageinfo.bucket,
+				Key: messageinfo.key
+			};
+			
+			
+			
+			var file = require('fs').createWriteStream('tmp/'+messageinfo.key.substring(10));
+			var requestt = s3.getObject(params2).createReadStream().pipe(file);
 				
-				//parametry do pobrania pliku (obiektu)
-				var params2 = {
+			requestt.on('finish', function (){
+					console.log('jestem tu po zapisaniu pliku na dysk');
+				}
+			);
+			
+			
+			//pobieramy plik (obiekt) i dane o nim
+			/*s3.getObject(params2, function(err, tekenFile) {
+			if (err) {
+				console.log(err, err.stack);
+			}
+			else {
+				
+				console.log("Plik zaladowany poprawnie.");
+				
+				//strumień pliku
+				//console.log(tekenFile.Body);
+				
+				//wrzuca przerobiony plik do processed
+				var paramsu = {
 					Bucket: messageinfo.bucket,
-					Key: messageinfo.key
+					Key: 'processed/'+messageinfo.key.substring(10),
+					ACL: 'public-read',
+					Body: tekenFile.Body,
 				};
-
-				//pobieramy plik (obiekt) i dane o nim
-				s3.getObject(params2, function(err, tekenFile) {
-					if (err) {
-						console.log(err, err.stack);
-					}
-					else {
-						console.log("Plik zaladowany poprawnie.");
-						//Wyliczenie skrótu czyki sumy kontrolnej dla pliku 
-						var algorithms = ['sha1', 'md5', 'sha256', 'sha512']
-						var loopCount = 1;
-						var doc = tekenFile.Body;
-						helpers.calculateMultiDigest(doc, algorithms, 
-							function(err, digests) {
-										
-	//WCIECIE JEST BO MNIE ODLEGLOSC TABULACJI DENERWOWALA
-	//A NIE CHCE MI SIE PRZERABIAC NA ODREBNE FUNKCJE :-)
-	console.log(digests);
+				s3.putObject(paramsu, function(err, datau) {
+				if (err) {
+					console.log(err, err.stack);
+				}
+				else {   
+					console.log(datau);
+					console.log('zuploadowano');
+				}
+				});
+			}
+			});
+				*/
+				
+				
+				
+				//Wyliczenie skrótu czyki sumy kontrolnej dla pliku 
+				//var algorithms = ['sha1', 'md5', 'sha256', 'sha512']
+				//var loopCount = 1;
+				//var doc = tekenFile.Body;
+				//helpers.calculateMultiDigest(doc, algorithms, function(err, digests) {
+				//	console.log(digests);
+				//}, loopCount);
 	
-	
-	
-	
+	/*
 	var paramsdb = {
 		Attributes: [
 			{ Name: messageinfo.key, Value: JSON.stringify(digests), Replace: true}
@@ -102,54 +141,13 @@ var myServer = function(){
 			});
 		}
 	});
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-
-	
-	
-	
-	
-	
-	
-	
-	
-	//zapis do pliku skrótów, później będzie do simpleDB
-	/*fs.writeFile("/home/bitnami/awslab4/actions/files/dane.s", digests, 
-		function(err) {
-			if(err) {
-				return console.log(err);
-			}
-			console.log("Zapisano do pliku");
-
-			//usuwanie wiadomosci z kolejki
-			var params = {
-			  QueueUrl: linkKolejki,
-			  ReceiptHandle: ReceiptHandle_forDelete
-			};
-			sqs.deleteMessage(params, function(err, data) {
-			  if (err) console.log(err, err.stack); // an error occurred
-			  else     console.log("Usunieto wiadomosc z kolejki: "+data);           // successful response
-			});
-	}); */
-							
-						}, loopCount);
-					}
-				});
-			}
+	*/
+		
 		}
+	}
 	});
-
 	setTimeout(myServer, 5000);
 }
+
 //odpalamy petle
 myServer();
